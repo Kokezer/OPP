a86d9da293af11bbdfc82fc601e32b17bc81ae1e
Kokezer <101249226+Kokezer@users.noreply.github.com>
2022-05-23 22:19:59 +0300

    Add files via upload

---
+++ Dz6/Dz/Dz.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+template<typename T> 
+
+void calc(T n, char c, T m) 
+{
+    
+    switch (c)
+    {
+   
+    case'+':cout << n << c << m << " = " << (n + m) << endl; break;
+    case'-': cout << n << c << m << " = " << (n - m) << endl; break;
+    case'/': cout << n << c << m << " = " << (n / m) << endl; break;
+    case'*': cout << n << c << m << " = " << (n * m) << endl; break;
+    default: cout << "Wrong" << endl;
+    }
+}
+int main() 
+{
+    
+    char a;
+    int c, b;
+    cout << " 1st number: \n";
+    cin >> c;
+    cout << " + | - | * | /  \n";
+    cin >> a;
+    cout << " 2st number:\n";
+    cin >> b;
+    calc(c, a, b);
+
+    return 0;
+}
